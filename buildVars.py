39b0c4f3a2103ddf63e3f8ec8ea0ef4cbdb8e4f6
Danstiv <50794055+Danstiv@users.noreply.github.com>
2026-05-25 14:31:00 +0700

    Update compatibility with nvda

--- buildVars.py
+++ buildVars.py
@@ -29,7 +29,7 @@ addon_info = {
 		"The settings are located in the NVDA menu -> Preferences -> Settings... -> URL player."
 	),
 	# version
-	"addon_version": "0.11.2",
+	"addon_version": "0.12.0",
 	# Author(s)
 	"addon_author": "Danstiv <danstiv@yandex.ru>",
 	# URL for the add-on documentation support
@@ -37,9 +37,9 @@ addon_info = {
 	# Documentation file name
 	"addon_docFileName": "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": '2024.1',
+	"addon_minimumNVDAVersion": '2026.1',
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": '2025.1',
+	"addon_lastTestedNVDAVersion": '2026.1',
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
